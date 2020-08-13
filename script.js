51db82169f6d7948e0ce03386e28448d0338d4be
Wisnu Anggoro <wisnu@anggoro.net>
2020-08-13 12:00:59 +0200

    Add helper function: setAttributes

--- script.js
+++ script.js
@@ -3,19 +3,30 @@ const loader = document.getElementById('loader');
 
 let photosArray = [];
 
+// Helper function to set attributes
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 // Create elements then display them
 function displayPhotos() {
     // Iterate through array's elements
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash website
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
         // Create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
         // Put <img> inside <a> then put in image-container
         item.appendChild(img);
         imageContainer.appendChild(item);
